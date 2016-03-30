5fd26e917b1600b6982893b568d8932e0780f71a
Jimmy O'Donnell <pearljim3@hotmail.com>
2016-03-30 11:16:32 -0700

    furthered data prep

--- Analysis/data_prep.R
+++ Analysis/data_prep.R
@@ -5,11 +5,19 @@ fig_dir <- file.path("..", "Figures")
 
 # generate a file of only the samples in the metadata
 # (assumes the metadata only has samples we're interested in rather than the full sequencing run)
+
+# holy macaroni, we need to decide on a standard orientation.
+# "OTUs_fam_w30_top10.csv" is in rows=OTU, cols=SAMPLES
+# YOU SHOULD PROBABLY DO THIS: start with script 'load_data.R',
+# use the resulting object 'otu_filt' in place of table_transposed
+# OR use t(otu_filt) as object 'table_full'
+
 filename_orig <- file.path(data_dir, "OTUs_fam_w30_top10.csv")
 
 filename_out <- file.path(data_dir, "OTUs_top10.csv")
 
-table_full <- read.csv(filename_orig, row.names = 1)
+# table_full <- read.csv(filename_orig, row.names = 1)
+# maybe ? table_full <- t(otu_filt)
 
 # load metadata
 metadata_file <- file.path(data_dir, "metadata_spatial.csv")
@@ -25,6 +33,8 @@ transect_line <- as.numeric(as.factor(paste(transect_line_rep, transect_position
 # what is the name of the column containing the sample id (SEQUENCING samples)
 colname_sampleid <- "sample_id"
 
+metadata_exp <- cbind.data.frame(metadata, sequenced_sample, transect_position, transect_line)
+
 # restrict to only samples in the metadata file
 # !!! it will really screw things up if you don't convert the sample id column to a character vector!
 table_restricted <- table_full[ , as.character(metadata[,colname_sampleid])]
@@ -34,13 +44,14 @@ write.csv(x = table_restricted, file = filename_out, quote = FALSE)
 
 # COMBINE UP DATA
 table_transposed <- t(table_restricted[,as.character(metadata[,colname_sampleid])])
+
 if(
   identical(
     rownames(table_transposed),
     metadata[,colname_sampleid]
   )
 ){
-  data_full <- cbind.data.frame(metadata, sequenced_sample, transect_position, transect_line, table_transposed)
+  data_full <- cbind.data.frame(metadata_exp, table_transposed)
   if(identical(rownames(data_full), data_full[,colname_sampleid])){
     rownames(data_full) <- NULL
   } else {
@@ -50,8 +61,23 @@ if(
   warning("something does not match up, and you could screw up the data big time by proceeding.")
 }
 
-data_full_path <- file.path(data_dir, "data_full_top10.csv")
+# data_full_path <- file.path(data_dir, "data_full_top10.csv")
+data_full_path <- file.path(data_dir, "data_full_filt.csv")
 write.csv(x = data_full, file = data_full_path, row.names = FALSE)
+
+# LONG FORMAT:
+otu_long <- melt(otu_filt)
+colnames(otu_long) <- c("sample_id", "OTU", "count")
+otu_long$OTU <- as.character(otu_long$OTU)
+otu_long$sample_id <- as.character(otu_long$sample_id)
+
+row_match_long <- match(otu_long$sample_id , metadata_exp[, colname_sampleid])
+
+data_full_long <- cbind.data.frame(metadata_exp[row_match_long,], otu_long[,c("OTU", "count")])
+
+data_full_long_path <- file.path(data_dir, "data_full_long.csv")
+write.csv(x = data_full_long, file = data_full_long_path, row.names = FALSE)
+
 
 
 # --------------- DATA TRIMMING ------------------------------------------
--- Analysis/load_data.R
+++ Analysis/load_data.R
@@ -5,13 +5,20 @@ fig_dir <- file.path("..", "Figures")
 
 # path to otu file: rows are samples, columns are OTUs, cells are counts.
 # rownames correspond to column "sample_id" in metadata
-otu_table_filename <- "otu_table_filtered_per_samp.csv"
+otu_table_filename <- "OTU_table.csv"
 
 # path to metadata file.
 # column "sample_id" contains rownames of otu file.
 metadata_filename <- "metadata_spatial.csv"
 
 otu_table <- read.csv(file.path(data_dir, otu_table_filename), row.names = 1, stringsAsFactors = FALSE)
+dim(otu_table)
+
+if(length(grep("lib_", colnames(otu_table))) > 0){
+	otu_table <- t(otu_table)
+}
+dim(otu_table)
+
 metadata <- read.csv(file.path(data_dir, metadata_filename), stringsAsFactors = FALSE)
 dist_along_shore <- c(1000,2000,0)[as.numeric(as.factor(metadata$transect))]
 metadata <- cbind.data.frame(metadata, dist_along_shore, stringsAsFactors = FALSE)
@@ -19,14 +26,40 @@ metadata <- cbind.data.frame(metadata, dist_along_shore, stringsAsFactors = FALS
 # exclude rows from OTU table that are not in metadata
 otu_table <- otu_table[metadata$sample_id,]
 
+# exclude OTUs not found in these samples
+otu_table <- otu_table[,which(colSums(otu_table) > 0)]
+dim(otu_table)
+
+# calculate the minimum number of reads assigned to these OTUs in these samples
+minreads <- min(rowSums(otu_table))
+
+# calculate proportional abundance of OTUs in each sample
 otu_table_prop <- otu_table/rowSums(otu_table)
 
-head(otu_table)[,1:10]
-head(metadata)
-metadata$sample_id
+# scale the proportional abundance of the OTU in each sample to the minimum number of reads
+otu_scaled <- otu_table_prop * minreads
+
+# ignore counts OTUs found fewer than 0.5 times (anything greater would be rounded to 1)
+otu_scaled[otu_scaled < 0.5] <- 0
+
+# round to whole numbers
+otu_scaled <- round(otu_scaled)
+dim(otu_scaled)
+
+# again exclude OTUs not found in these samples
+otu_scaled <- otu_scaled[,which(colSums(otu_scaled) > 0)]
+dim(otu_scaled)
+
+# to re-order by the abundance in THESE samples (i.e. not those from samples from elsewhere)
+otu_scaled <- otu_scaled[,order(colSums(otu_scaled), decreasing = TRUE)]
+otu_scaled[1:10,1:10]
+
+# exclude OTUs that were counted a total of fewer than 100 times across samples
+otu_filt <- otu_scaled[,which(colSums(otu_scaled) > 100)]
+dim(otu_filt)
 
 
-boxplot(otu_table[,1:20])
+boxplot(otu_filt[,1:20])
 # boxplot(log(otu_table[,1:20]))
 boxplot(otu_table_prop[,1:20])
 boxplot(scale(otu_table[,1:20]))
