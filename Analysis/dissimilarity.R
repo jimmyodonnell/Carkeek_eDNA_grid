eeb888df5907972daec385fc5fa0b7edb9520729
Jimmy O'Donnell <pearljim3@hotmail.com>
2016-04-11 21:28:49 -0700

    fix filtering of subsamples on dissimilarity basis

--- Analysis/dissimilarity.R
+++ Analysis/dissimilarity.R
@@ -4,30 +4,34 @@ library(vegan) # vegdist
 # rownames must be unique sample IDs. get this from 1_data_prep.R
 my_table <- otu_filt
 
-vegdist(otu_filt)
+if(!(identical(metadata[,colname_sampleid], rownames(my_table)))){
+	warning("hold on, the otu and metadata rows are not in the same order; disaster could ensue")
+}
 # vector of groups to which samples belong.
 group_vector <- metadata$env_sample_name
 
+
 dis_by_sample <- function(contingency_table, grouping_vector)
 {
-  return(
-    lapply(
-      lapply(
-        split(contingency_table, grouping_vector), 
-        matrix, ncol = ncol(contingency_table)
-      ), 
+dis_by_group <- lapply(
+      # lapply(
+        split(as.data.frame(contingency_table), grouping_vector), 
+        # matrix, ncol = ncol(contingency_table)
+      # ), 
       vegdist, method = "bray", upper = TRUE, diag = TRUE
     )
-  )
+  return(dis_by_group)
 }
 
 my_dis <- dis_by_sample(my_table, group_vector)
 
-which.max(rowSums(do.call(rbind, lapply(my_dis, function(x) rowMeans(as.matrix(x))))))
-which.max(do.call(rbind, lapply(my_dis, function(x) rowMeans(as.matrix(x)))))
+# calculate mean dissimilarity
+dis_mean <- lapply(my_dis, function(x) rowMeans(as.matrix(x)))
 
-metadata[,"sample_id"][5]
-my_dis[[1]]
+# Which samples have high dissimilarity among other samples from the same environmental sample?
+dis_mean_v <- unlist(dis_mean)
+names(dis_mean_v) <- do.call(c, lapply(dis_mean, names))
+names(which.max(dis_mean_v))
 
 par(mar = c(4,6,1,1))
 stripchart(my_dis, method = "jitter", pch = 21, las = 1, 
@@ -42,5 +46,4 @@ stripchart(my_dis, method = "jitter", pch = 21, las = 1,
 ####################################################################################
 # ALTERNATE
 # Use on mean OTU abundance per water sample (i.e. don't compare among PCR replicates)
-
 vegdist(otu_mean, method = "bray", binary = FALSE)
