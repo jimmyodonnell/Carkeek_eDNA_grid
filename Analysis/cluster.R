606e7a36f97e1ff9d19fb3a5d3c58b464f1f00d5
Jimmy O'Donnell <pearljim3@hotmail.com>
2016-04-13 16:53:25 -0700

    improve PAM output

--- Analysis/cluster.R
+++ Analysis/cluster.R
@@ -13,11 +13,11 @@ gghue <- function(n){
 
 # choose between using proportional or raw counts
 # DATA <- otu_table
-DATA <- otu_filt[,]
+# DATA <- otu_filt[,]
 DATA <- otu_mean
 metadata <- metadata_exp[!duplicated(metadata_exp[,"env_sample_name"]),]
 
-mydist <- vegdist(DATA, binary = TRUE)
+mydist <- vegdist(DATA, method = "bray") # , binary = TRUE
 
 mypam <- pamk(data = mydist) # to restrict range of Ks considered: , krange = 2:4
 pam_out <- mypam$pamobject
@@ -28,7 +28,7 @@ plot(pam(mydist, k = mypam$nc), which.plots = 1)
 mycolors <- gghue(mypam$nc)
 
 # see ?clusplot.default
-# pdf(file = file.path(fig_dir, "pam_plot.pdf"))
+pdf(file = file.path(fig_dir, "pam_plot.pdf"))
 	par(mar = c(5,5,1,1))
 	clusplot(
 		x = as.matrix(mydist), 
@@ -42,7 +42,7 @@ mycolors <- gghue(mypam$nc)
 		col.clus = mycolors, 
 		main = NA
 	)
-# dev.off()
+dev.off()
 
 
 set.seed(2) # to control jitter
@@ -70,7 +70,7 @@ set.seed(2) # to control jitter
 
 
 # USING MEAN DATA
-# pdf(file = file.path(fig_dir, "medoids_in_space_mean.pdf"), width = 7, height = 7)
+pdf(file = file.path(fig_dir, "pam_in_space.pdf"), width = 5, height = 7)
 plot(
 		x = metadata$dist_along_shore, 
 		xaxt = "n", 
@@ -80,7 +80,24 @@ plot(
 		# log = "y",
 		col = mycolors[mypam$pamobject$clustering[metadata$env_sample_name]], 
 		# bg = mycolors[mypam$pamobject$clustering[metadata$sample_id]], 
-		pch = as.character(mypam$pamobject$clustering[metadata$env_sample_name]-1), 
+		pch = 19, #as.character(mypam$pamobject$clustering[metadata$env_sample_name]-1)
+		cex = 4, 
+		# cex = 1.5, 
+		main = "membership to PAM classifications", 
+		las = 1, 
+		xlab = "Position along shore (meters)", 
+		ylab = "Position from 0 (meters)"
+	)
+points(
+		x = metadata$dist_along_shore, 
+		xaxt = "n", 
+		xlim = c(-500, 2500), 
+		y = log(metadata$dist_from_shore + 10), 
+		yaxt = "n",
+		# log = "y",
+		col = "white", 
+		# bg = mycolors[mypam$pamobject$clustering[metadata$sample_id]], 
+		pch = as.character(mypam$pamobject$clustering[metadata$env_sample_name]), 
 		cex = 2, 
 		# cex = 1.5, 
 		main = "membership to PAM classifications", 
@@ -90,4 +107,4 @@ plot(
 	)
 	axis(side = 2, at = unique(log(metadata$dist_from_shore + 10)), labels = unique(metadata$dist_from_shore), las = 1)
 	axis(side = 1, at = unique(metadata$dist_along_shore), labels = unique(metadata$dist_along_shore), las = 1)
-# dev.off()
+dev.off()
