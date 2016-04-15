dcc76aad5f04fee1fc5f81c9a0a3ddfbe6c9e4ec
Jimmy O'Donnell <pearljim3@hotmail.com>
2016-04-14 18:15:33 -0700

    add more variogram plots

--- Analysis/variogram.R
+++ Analysis/variogram.R
@@ -3,7 +3,8 @@ my_metadata <- metadata_mean
 
 library(geoR) # variog
 
-pdf(file = file.path(fig_dir, "variograms_taxa.pdf"))
+# variogram for individual taxon abundance
+pdf(file = file.path(fig_dir, "variogram_taxa.pdf"))
 for(i in 1:20){
 
 	vg <- variog(coords = my_metadata[,c(colname_lon, colname_lat)], data = my_table[,i])
@@ -12,8 +13,22 @@ for(i in 1:20){
 
 }
 dev.off()
-# vg.summary <- cbind(c(1:10), v2$v, v2$n)
-# colnames(v2.summary) <- c("lag", "semi-variance", "# of pairs")
-# v2.summary
 
-# plot(v2, type = "b", main = "Variogram: Av8top")
+
+# variogram for shannon index
+pdf(file = file.path(fig_dir, "variogram_shannon.pdf"))
+shannon <- diversity(my_table, index = "shannon")
+vg <- variog(coords = my_metadata[,c(colname_lon, colname_lat)], data = my_table[,i])
+plot(vg, type = "b", main = "Variogram of Shannon Index")
+dev.off()
+
+
+# variogram for taxon richness (number of taxa w/ mean abundance > 1)
+thresholds <- c(0, 1, 10, 100, 1000, 10000)
+pdf(file = file.path(fig_dir, "variogram_richness.pdf"))
+for(i in 1:length(thresholds)){
+	richness <- rowSums(my_table > i)
+	vg <- variog(coords = my_metadata[,c(colname_lon, colname_lat)], data = richness)
+	plot(vg, type = "b", main = paste("Variogram of Richness, abundance threshold = ", thresholds[i]))
+}
+dev.off()
