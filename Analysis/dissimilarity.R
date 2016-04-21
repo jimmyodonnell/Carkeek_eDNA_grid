8b928d1479d5b227344db49e50bd2edab2f9c22d
Jimmy O'Donnell <pearljim3@hotmail.com>
2016-04-20 17:32:28 -0700

    improvements to make dissimilarity standalone

--- Analysis/dissimilarity.R
+++ Analysis/dissimilarity.R
@@ -4,6 +4,10 @@ library(vegan) # vegdist
 # rownames must be unique sample IDs. get this from 1_data_prep.R
 my_table <- otu_filt
 
+# At how many standard deviations above the mean dissimilarity
+# should a sample be excluded?
+THRESHOLD_SD <- 1
+
 if(!(identical(metadata[,colname_sampleid], rownames(my_table)))){
 	warning("hold on, the otu and metadata rows are not in the same order; disaster could ensue")
 }
@@ -27,44 +31,61 @@ dis_by_group <- lapply(
 }
 
 my_dis <- dis_by_sample(my_table, group_vector)
-
-# which sample has the highest BC dissimilarity?
-dis_high <- which.max(sapply(my_dis, max))
-as.dist(my_dis[[dis_high]], upper = FALSE, diag = FALSE)
-mean(as.dist(my_dis[[dis_high]], upper = FALSE, diag = FALSE)[1:3])
+# unlist seems to conveniently remove 0s and redundant values for distance matrices
+my_dis_v <- unlist(my_dis)
 
 # calculate mean dissimilarity
-dis_mean <- lapply(my_dis, function(x) rowMeans(as.matrix(x)))
+dis_mean <- mean(my_dis_v)
+dis_sd <- sd(my_dis_v)
+dissimilarity_cutoff <- dis_mean + (dis_sd * THRESHOLD_SD)
 
-# Which samples have high dissimilarity among other samples from the same environmental sample?
-dis_mean_v <- unlist(dis_mean)
-names(dis_mean_v) <- do.call(c, lapply(dis_mean, names))
-
-# One "bad" replicate will have a high mean BC dissimilarity to all other replicates, but it will also elevate the BC dissimilarity of the other replicates
-round(sort(dis_mean_v), digits = 3)
-plot(dis_mean_v)
 # pdf(file = file.path(fig_dir, "BC_before_cleaning.pdf"))
 par(mar = c(4,7,1,1))
-stripchart(my_dis, method = "jitter", pch = 21, las = 1, 
-           xlab = "Pairwise Bray-Curtis Dissimilarity", bg = rgb(0,0,0,alpha = 0.2))
-# dev.off()
+stripchart(my_dis, method = "jitter", pch = 21, xlim = c(0,1), 
+           xlab = "Pairwise Bray-Curtis Dissimilarity", 
+           bg = rgb(0,0,0,alpha = 0.2), las = 1)
+
+
+# plot all
+# plot(my_dis_v, ylim = c(0,1), 
+	# ylab = "Within-sample dissimilarity (Bray-Curtis)", 
+	# xlab = "arbitrary sample index",
+	# las = 1)
+line_colors <- c("black", "red")
+abline(v = c(dis_mean, dissimilarity_cutoff), 
+	lty = 2, col = line_colors)
+	
+legend("topright", legend = c("mean", "threshold"), 
+	bty = "n", lty = 2, col = line_colors)
 
 # note high value for sample "lib_B_tag_GCGCTC" in PCT-C-0500
-# in this case, 0.2 seems to be a reasonable cutoff beyond which we think a PCR replicate is "bad"
-names(which(dis_mean_v > 0.2))
-names(which.max(dis_mean_v))
+# dev.off()
+
+# which "environmental sample" contains dissimilarities over the threshold?
+bad_env_sample <- which(
+	sapply(
+		lapply(my_dis, 
+		function(x) which(x > dissimilarity_cutoff)
+		), length) > 0)
+
+
+# Which samples have high dissimilarity among other samples from the same environmental sample?
+# what are the names of those bad comparisons within that sample
+dis_names <- rownames(which(as.matrix(my_dis[[bad_env_sample]]) > dissimilarity_cutoff, arr.ind = TRUE))
+
+# what is the name of the replicate occuring more than once?
+bad_replicate_name <- names(table(dis_names))[table(dis_names) > 1]
 
 # remove that sample?
-remove_outlier <- TRUE
-if(remove_outlier == TRUE){
-  outlier <- names(which(dis_mean_v > 0.2))
-  metadata <- metadata[metadata$sample_id != outlier,]
-  metadata_exp <- metadata_exp[metadata_exp$sample_id != outlier,]
-  otu_filt <- otu_filt[rownames(otu_filt) != outlier,]
+remove_bad_replicate <- TRUE
+if(remove_bad_replicate == TRUE){
+  metadata <- metadata[metadata$sample_id != bad_replicate_name,]
+  metadata_exp <- metadata_exp[metadata_exp$sample_id != bad_replicate_name,]
+  my_table_cleaned <- my_table[rownames(my_table) != bad_replicate_name,]
   print("removed outlier PCR")
 }
 
-my_table <- otu_filt
+my_table <- my_table_cleaned
 if(!(identical(metadata[,colname_sampleid], rownames(my_table)))){
 	warning("hold on, the otu and metadata rows are not in the same order; disaster could ensue")
 }
