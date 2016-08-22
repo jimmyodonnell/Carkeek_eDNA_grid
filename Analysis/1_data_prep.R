174edad7fdc556e9db11e2d569a94107e3e0ef53
Jimmy O'Donnell <jodonnellbio@gmail.com>
2016-08-22 14:16:38 -0700

    correct funky function syntax

--- Analysis/1_data_prep.R
+++ Analysis/1_data_prep.R
@@ -18,7 +18,9 @@
 # some_tables[["yet_another"]] <- matrix(data = rnorm(25), nrow = 5)
 
 # this function rescales a numeric vector to 0 and 1
-scale01 <- function (x) {(x-min(x))/(max(x)-min(x))}
+scale01 <- function (x) {
+  (x-min(x))/(max(x)-min(x))
+}
 
 # this function rescales a matrix to contain only 0 and 1
 as.binary <- function (a_matrix) {
@@ -27,26 +29,22 @@ as.binary <- function (a_matrix) {
 	return(bin_mat)
 }
 
-strip_absent <- function(x)
-{
+strip_absent <- function(x) {
 # this function removes any columns for which the sum is <= 0
 	return(x[,which(colSums(x) > 0)])
 }
 
-present_in_all_rows <- function(x)
-{
+present_in_all_rows <- function(x) {
 # this function removes columns which are not > 0 in all rows
 	return(x[ , colSums(x > 0) >= nrow(x)])
 }
 
-not_in_all_rows <- function(x)
-{
+not_in_all_rows <- function(x) {
 # this function removes columns which are > 0 in all rows
 	return(x[ , colSums(x > 0) < nrow(x)])
 }
 
-prop <- function(x)
-{
+prop <- function(x) {
 # convert a matrix of counts to proportions of the samples
 	return(x/rowSums(x))
 }
