ae67ad4ede8d33fe8721f851b259c0de79e7ec48
Jimmy O'Donnell <jodonnellbio@gmail.com>
2016-09-29 17:15:40 -0700

    finish diversity plotting

--- Analysis/diversity.R
+++ Analysis/diversity.R
@@ -2,41 +2,75 @@ library(vegan)
 
 # requires a list of OTU tables and a list of corresponding metadata
 
-shannon <- lapply(otu_table, diversity, index = "shannon") 
-simpson <- lapply(otu_table, diversity, index = "simpson") 
-# "shannon", "simpson", "invsimpson"
+div_metrics <- list()
 
-richness <- lapply(otu_table, function(x) {rowSums(x > 0)})
+div_metrics[["Shannon"]] <- lapply(otu_table, diversity, index = "shannon") 
+div_metrics[["Simpson"]] <- lapply(otu_table, diversity, index = "simpson") 
+div_metrics[["Richness"]] <- lapply(otu_table, function(x) {rowSums(x > 0)})
 
+which_data <- "mean"
 
-which_data <- "mean_unfilt"
+plot_name <- "diversity"
+# if(!exists("legend_text")){legend_text <- list()}
+# legend_text[plot_name] <- {"legend text goes here"}
 
-plot_dat <- simpson[[which_data]][
+if(EXPORT){
+  pdf_file    <- file.path(fig_dir, paste(plot_name, ".pdf", sep = ""))
+  # legend_file <- file.path(fig_dir, paste(plot_name, "_legend.txt", sep = ""))
+  # writeLines(legend_text[[plot_name]], con = legend_file)
+  pdf(file = pdf_file, width = 8, height = 4) #, width = 8, height = 3
+}
+
+par(mfrow = c(1,length(div_metrics)))
+
+for(i in 1:length(div_metrics)){
+
+plot_dat <- div_metrics[[i]][[which_data]][
   metadata[[which_data]][,colname_env_sample]
 ]
 
 n_colors <- 10
 #Create a function to generate a continuous color palette
-colorpalette <- colorRampPalette(c('blue','red'))
+if(require(viridis)){
+  colorpalette <- viridis
+} else {
+  colorpalette <- colorRampPalette(c('darkorchid','aquamarine'))
+}
 
 colorified <- colorpalette(n_colors)[cut(plot_dat, breaks = n_colors)]
 
-par(mar = c(4,5,1,3))
+y_for_plotting <- as.numeric(as.factor(my_metadata[ , colname_ycoord]))
+
+par(mar = c(4,5,3,3))
 plot(
-  x = 
-metadata[[which_data]][,colname_xcoord], 
-  y = 
-metadata[[which_data]][,colname_ycoord], 
+  x = metadata[[which_data]][,colname_xcoord], 
+  xlim = c(-500, 2500), # add a little space to the right and left
+  y = y_for_plotting, 
   col = colorified,
   pch = 19, 
   cex = 3, 
-  xlab = "Transect", 
-  ylab = "Distance from 0", 
+  xlab = "", 
+  ylab = "", 
   axes = FALSE, 
   las = 1
 )
-axis(2, las = 1)
+if(i == 1){
+  axis(side = 2, at = unique(y_for_plotting), 
+    labels = unique(my_metadata[,colname_ycoord]), las = 1)
+  title(ylab = "Distance from 0 (meters)")
+}
+
+title(main = names(div_metrics[i]))
+title(xlab = "Transect", line = 1)
+
+
 
 # add color legend
 colvec <- colorpalette(n_colors)[cut(sort(plot_dat), breaks = n_colors)]
 color_legend(col = colvec, lev = sort(plot_dat))
+
+
+}
+if(EXPORT){
+  dev.off()
+}
