b455e0bd625bbf5126a93474980ab249b048d051
Jimmy O'Donnell <jodonnellbio@gmail.com>
2016-11-02 16:50:29 -0700

    fix plotting of diversity

--- Analysis/diversity.R
+++ Analysis/diversity.R
@@ -2,7 +2,7 @@ library(vegan)
 
 # requires a list of OTU tables and a list of corresponding metadata
 
-EXPORT <- FALSE
+EXPORT <- TRUE
 
 div_metrics <- list()
 
@@ -13,17 +13,19 @@ div_metrics[["Richness"]] <- lapply(otu_table, function(x) {rowSums(x > 0)})
 which_data <- "mean"
 
 plot_name <- "diversity"
-# if(!exists("legend_text")){legend_text <- list()}
-# legend_text[plot_name] <- {"legend text goes here"}
+if(!exists("legend_text")){legend_text <- list()}
+legend_text[plot_name] <- {"Aggregate measures of diversity at each sample site."}
 
 if(EXPORT){
   pdf_file    <- file.path(fig_dir, paste(plot_name, ".pdf", sep = ""))
-  # legend_file <- file.path(fig_dir, paste(plot_name, "_legend.txt", sep = ""))
-  # writeLines(legend_text[[plot_name]], con = legend_file)
-  pdf(file = pdf_file, width = 8, height = 4) #, width = 8, height = 3
+  legend_file <- file.path(fig_dir, paste(plot_name, "_legend.txt", sep = ""))
+  writeLines(legend_text[[plot_name]], con = legend_file)
+  pdf(file = pdf_file, width = 5, height = 4) #, width = 8, height = 3
 }
 
-par(mfrow = c(1,length(div_metrics)))
+the_layout <- layout(mat = matrix(c(1,2), nrow = 1), widths = c(11,6))
+
+margins_subplot <- list(c(4,5,3,5), c(4,0,3,3), c(4,0,3,3))
 
 for(i in 1:length(div_metrics)){
 
@@ -43,7 +45,7 @@ colorified <- colorpalette(n_colors)[cut(plot_dat, breaks = n_colors)]
 
 y_for_plotting <- as.numeric(as.factor(metadata[[which_data]][ , colname_ycoord]))
 
-par(mar = c(4,5,3,3))
+par(mar = margins_subplot[[i]], xpd = TRUE)
 plot(
   x = metadata[[which_data]][,colname_xcoord], 
   xlim = c(-500, 2500), # add a little space to the right and left
@@ -65,10 +67,9 @@ if(i == 1){
 title(main = names(div_metrics[i]))
 title(xlab = "Transect", line = 1)
 
-
-
 # add color legend
-colvec <- colorpalette(n_colors)[cut(sort(plot_dat), breaks = n_colors)]
+dat_col_leg <- seq(min(plot_dat), max(plot_dat), length.out = n_colors)
+colvec <- colorpalette(n_colors)[cut(dat_col_leg, breaks = n_colors)]
 color_legend(col = colvec, lev = sort(plot_dat))
 
 
