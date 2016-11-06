0d30028e3661ef917aae57c317c1f2a6bb1599fb
Jimmy O'Donnell <jodonnellbio@gmail.com>
2016-11-06 12:30:15 -0800

    require first argument of plot_model point to confidence interval data frame

--- Analysis/functions/plot_model.R
+++ Analysis/functions/plot_model.R
@@ -1,22 +1,22 @@
-plot_model <- function(model_list_item, pred_vec, 
-  line_color = "indianred", 
-  line_type  = 3, 
+plot_model <- function(confidence_df, pred_vec,
+  line_color = "indianred",
+  line_type  = 3,
   band_color = hsv(h = 1, s = 1, v = 0.1, alpha = 0.2)
   ){
-  if(length(model_list_item$conf) > 0) {
-    x_bounds <- c(x_pred, rev(x_pred))
-    conf     <- model_list_item$conf
+  if(length(confidence_df) > 0) {
+    x_bounds <- c(pred_vec, rev(pred_vec))
+    conf     <- confidence_df
     y_bounds <- c(conf[,"lwr"], rev(conf[,"upr"]))
     polygon(
-      x = x_bounds, 
+      x = x_bounds,
       y = y_bounds,
-      col = band_color, 
+      col = band_color,
       border = NA
     )
   } else {
     warning("Object you pointed to does not have confidence bounds")
   }
     # add fit line
-    lines(x = x_pred, y = model_list_item$conf[,"fit"], 
+    lines(x = pred_vec, y = confidence_df[,"fit"],
         col = line_color, lwd = 3, lty = line_type)
 }
